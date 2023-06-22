fae6a27515fe4b771dc64c565617340dd27990f3
Xaphan67 <cedric.falda@gmail.com>
2023-06-22 09:22:30 +0200

    Exercice 14 - avec Datetime

--- exo14.php
+++ exo14.php
@@ -6,13 +6,20 @@
 
 <?php
 
-echo "La date de naissance de la personne est le 17/01/1985 :";
+echo "La date de naissance de la personne est le 17/01/1985 :</br>";
 
 $maintenant = time(); // Temps à l'instant ou le code est executé
 $date_naissance = strtotime("1985-01-17"); // Temps à l'instant de la date de naissance de la personne
 $temps = $maintenant - $date_naissance; // Temps entre les deux dates
+echo "difference temps : $temps</br>";
 
-$temps_jours = round($temps / (60 * 60 * 24)); // Secondes * minutes * heures
+$années = floor($temps / (365 * 24 * 60 * 60)); // 365 jours * 24 heures * 60 minutes * 60 secondes dans une année
+$temps -= $années * 365 * 24 * 60 * 60; // Retire le temps des années au temps total
 
-echo $mois = intval($temps_jours / 30);
+$mois = floor($temps / (30 * 24 * 60 * 60)); // 30 jours * 24 heures * 60 minutes * 60 secondes dans un mois
+$temps -= $mois * 30 * 24 * 60 * 60; // Retire le temps des mois au temps total
+
+$jours = floor($temps / (24 * 60 * 60)); // 24 heures * 60 minutes * 60 secondes dans un jour
+
+echo "Âge de la personne : $années ans $mois mois $jours jours";
 
