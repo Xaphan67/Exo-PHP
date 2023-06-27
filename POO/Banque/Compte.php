ffa291a541b63584387ef9f6366804c4bcf1a7e6
Xaphan67 <cedric.falda@gmail.com>
2023-06-27 10:51:07 +0200

    Exo Banque

---
+++ POO/Banque/Compte.php
@@ -0,0 +1,62 @@
+<?php
+
+class Compte
+{
+    private string $_libelle;
+    private int $_solde;
+    private string $_devise;
+    private Titulaire $_titulaire;
+
+    public function __construct(string $libelle, int $solde, string $devise, Titulaire $titulaire)
+    {
+        $this->$_libelle = $libelle;
+        $this->$_solde = $solde;
+        $this->$_devise = $devise;
+        $this->$_titulaire = $titulaire;
+    }
+
+    public getLibelle()
+    {
+        return $this->_libelle;
+    }
+
+    public setLibelle($libelle)
+    {
+        $this->_libelle = =$libelle;
+    }
+
+    public getSolde()
+    {
+        return $this->_solde;
+    }
+
+    public setSolde($solde)
+    {
+        $this->_solde = =$solde;
+    }
+
+    public getDevise()
+    {
+        return $this->_devise;
+    }
+
+    public setDevise($devise)
+    {
+        $this->_devise = =$devise;
+    }
+
+    public getTitulaire()
+    {
+        return $this->_titulaire;
+    }
+
+    public setTitulaire($titulaire)
+    {
+        $this->_titulaire = =$titulaire;
+    }
+
+    public function __toString()
+    {
+        return $this->getLibelle() . " de " . $this->getTitulaire()->getNom() . " " . $this->getTitulaire()->getPrenom() . " - Solde : " . $this->getSolde() . " " . $this->getDevise();
+    }
+}
